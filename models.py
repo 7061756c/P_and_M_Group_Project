def52c253ed8df392009341c14a9269b2e9b0cea
7061756c <pphillip@gmail.com>
2019-07-22 19:54:38 -0500

    Update models.py

--- models.py
+++ models.py
@@ -32,3 +32,34 @@ class Ninjas(db.Model):
   created_at = db.Column(db.DateTime, server_default=func.now())
   updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
 
+class Products(db.Model):
+  __tablename__ = "products"
+  id = db.Column(db.Integer, primary_key=True)
+  p_name = db.Column(db.String(45))
+  desc = db.Column(db.String(45))
+  cost = db.Column(db.Integer())
+  created_at = db.Column(db.DateTime, server_default=func.now())
+  updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
+  
+class Orders(db.Model):
+  __tablename__ = "orders"
+  id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.Integer, db.ForeignKey('dojos.id'), nullable = False)
+  is_complete = db.Column(db.String(45))
+  created_at = db.Column(db.DateTime, server_default=func.now())
+  updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
+  
+class Items(db.Model):
+  __tablename__ = "orders_items"
+  id = db.Column(db.Integer, primary_key=True)
+  p_id = db.Column(db.Integer, db.ForeignKey('dojos.id'), nullable = False)
+  quantity = db.Column(db.Integer())
+  created_at = db.Column(db.DateTime, server_default=func.now())
+  updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
+
+class Coupons(db.Model):
+  __tablename__ = "coupons"
+  id = db.Column(db.Integer, primary_key=True)
+  code = db.Column(db.Integer())
+  created_at = db.Column(db.DateTime, server_default=func.now())
+  updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
